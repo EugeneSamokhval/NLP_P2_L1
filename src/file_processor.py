65a64572d739b4fc4eb4bce3d14fbf0e2cb079f0
EugeneSamokhval <91789484+EugeneSamokhval@users.noreply.github.com>
2024-09-26 10:17:02 +0300

    Changes to be committed: 	new file:   src/config.json 	new file:   src/file_processor.py 	modified:   src/main.py

---
+++ src/file_processor.py
@@ -0,0 +1,43 @@
+import tempfile
+import json
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import fitz  # PyMuPDF
+from docx import Document
+
+
+def get_raw_text(file, path: str):
+    stop_words = set(stopwords.words('english'))
+
+    def process_text(text):
+        words = word_tokenize(text)
+        filtered_words = {word: words.count(
+            word) for word in words if word.lower() not in stop_words}
+        return filtered_words
+
+    if path.endswith('.txt'):
+        filecontent = file.read()
+        return process_text(filecontent.decode('utf-8'))
+
+    elif path.endswith('.pdf'):
+        with tempfile.TemporaryFile() as temp_pdf:
+            temp_pdf.write(file.read())
+            temp_pdf.seek(0)
+            doc = fitz.open(stream=temp_pdf.read(), filetype="pdf")
+            text = ""
+            for page in doc:
+                text += page.get_text()
+        return process_text(text)
+
+    elif path.endswith('.docx'):
+        with tempfile.TemporaryFile() as temp_docx:
+            temp_docx.write(file.read())
+            temp_docx.seek(0)
+            doc = Document(temp_docx)
+            text = ""
+            for paragraph in doc.paragraphs:
+                text += paragraph.text + "\n"
+        return process_text(text)
+
+    else:
+        raise ValueError("Unsupported file type")
--- src/main.py
+++ src/main.py
@@ -1,22 +1,33 @@
 from fastapi import FastAPI, File, UploadFile, Form
-import shutil
+import json
 import gridfs
+import file_processor
 from pymongo import MongoClient
+import nltk
+
+nltk.download('stopwords')
+nltk.download('punkt_tab')
 
 app = FastAPI()
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client['SearchingProfiles']
 fs = gridfs.GridFS(db)
+with open('./config.json', 'r') as config_file:
+    config = json.load(config_file)
 
 
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...), path: str = Form(...)):
-    file_id = fs.put(file.file, filename=file.filename, path=path)
+    raw_text_dict = file_processor.get_raw_text(file.file, path)
+    # Convert dictionary to JSON string
+    raw_text_json = json.dumps(raw_text_dict)
+    file_id = fs.put(raw_text_json.encode('utf-8'),
+                     filename=file.filename, path=path)
     return {"filename": file.filename, "path": path, "file_id": str(file_id)}
-
 
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run(app="main:app", host='localhost', port=2000, reload=True)
+    uvicorn.run(app="main:app",
+                host=config['host'], port=config['port'], reload=True)
