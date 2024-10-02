63c26c8826d95b1c3bef2e5a4b5050f2e516804c
EugeneSamokhval <91789484+EugeneSamokhval@users.noreply.github.com>
2024-10-02 12:42:51 +0300

    Changes to be committed: modified:   client/config.json new file:   src/ai_feature.py modified:   src/main.py

---
+++ src/ai_feature.py
@@ -0,0 +1,26 @@
+from nltk.corpus import stopwords
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+
+def parse_query(query):
+    doc = nlp(query)
+
+    include_terms = []
+    exclude_terms = []
+    stop_words = set(stopwords.words('english'))
+    for token in doc:
+        if token.text.lower() == "and":
+            continue
+        elif token.text.lower() == "without":
+            exclude_terms.append(next(doc[token.i + 1:]).text)
+        else:
+            include_terms.append(token.text)
+
+    include_terms = [
+        term for term in include_terms if term not in exclude_terms]
+    stop_words = set(stopwords.words('english'))
+    include_terms = [term for term in include_terms if term not in stop_words]
+    exclude_terms = [term for term in exclude_terms if term not in stop_words]
+    return include_terms, exclude_terms
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import gridfs
 import file_processor
 from pymongo import MongoClient
 import nltk
+import ai_feature
 
 nltk.download('stopwords')
 nltk.download('punkt_tab')
@@ -16,6 +17,8 @@ fs = gridfs.GridFS(db)
 with open('./config.json', 'r') as config_file:
     config = json.load(config_file)
 
+db['documents'].create_index([('field_name', 'text')])
+
 
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...), path: str = Form(...)):
@@ -25,6 +28,28 @@ async def upload_file(file: UploadFile = File(...), path: str = Form(...)):
     file_id = fs.put(raw_text_json.encode('utf-8'),
                      filename=file.filename, path=path)
     return {"filename": file.filename, "path": path, "file_id": str(file_id)}
+
+
+@app.get("/find")
+async def find_file(user_input: str):
+    include_terms, exclude_terms = ai_feature.parse_query(user_input)
+
+    collumn = db['documents']
+    # Create MongoDB query
+    include_query = " ".join(include_terms)
+    query = {
+        "$text": {"$search": include_query},
+    }
+
+    if exclude_terms:
+        query["$nor"] = [{"$text": {"$search": term}}
+                         for term in exclude_terms]
+    try:
+        results = collumn.find(query)
+        print(list(results))
+        return list(results)
+    except Exception as e:
+        return {"error": str(e)}
 
 if __name__ == '__main__':
     import uvicorn
